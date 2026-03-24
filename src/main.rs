6d27592c36d3d6a8c7f3cf45530f4899f8a152af
Darkx32 <matheusgomessantos835@gmail.com>
2026-03-24 00:36:40 -0300

    fix: fixed bug on commit

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,24 @@ mod arg;
 mod ui;
 mod core;
 
-fn main() -> color_eyre::Result<()>{
-    color_eyre::install()?;
+use owo_colors::OwoColorize;
 
+fn main() {
+    #[cfg(debug_assertions)]
+    color_eyre::install().unwrap();
+
+    if let Err(err) = run() {
+        #[cfg(debug_assertions)]
+        eprintln!("{:?}", err.red());
+
+        #[cfg(not(debug_assertions))]
+        eprintln!("{}", err.red());
+
+        std::process::exit(1);
+    }
+    
+}
+
+fn run() -> color_eyre::Result<()> {
     cli_handle()
 }
